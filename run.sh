d5f1dd4b8a72d2d7833d58736662a034538c2212
Abhishek Thakur <a.thakur@searchmetrics.com>
2016-05-03 10:33:38 +0200

    update run

--- run.sh
+++ run.sh
@@ -1,3 +1,22 @@
-echo "Dumping datasets as pickle files"
+SECONDS=0
+echo "Dumping datasets as pickles"
+echo "This makes our lives easy"
+echo "Time Elapsed: ", $SECONDS
 python dump_datasets.py
-
+echo "Time Elapsed: ", $SECONDS
+echo "Training Evita"
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python evita.py
+echo "Time Elapsed: ", $SECONDS
+echo "Training flora"
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python flora.py
+echo "Time Elapsed: ", $SECONDS
+echo "Training helena"
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python helena.py
+echo "Time Elapsed: ", $SECONDS
+echo "Training tania"
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python tania.py
+echo "Time Elapsed: ", $SECONDS
+echo "Training yolanda"
+THEANO_FLAGS=mode=FAST_RUN,device=gpu,floatX=float32 python yolanda.py
+echo "Time Elapsed: ", $SECONDS
+echo "Training complete. All files can be found in res/"
